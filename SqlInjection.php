1e7958c66e9e0ad09db0f8ed8d16aa6ddf68d4a3
Budhi Octaviansyah <budioct@gmail.com>
2024-05-15 17:41:10 +0700

    sql injection di php

---
+++ SqlInjection.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/ConnectionDB.php'; // require_once __DIR__ . '/namaClass.php';
+
+$PDO = getConnection(); // open koneksi ke driver mysql
+
+$username = "budioct'; #"; // karna # jadi di belakang query di angap comment
+$password = "salah gak peduli"; // jadi comment karna variable username ada #
+
+$sql = "SELECT * FROM admin WHERE username = '$username' AND password = '$password';"; // query tidak di sarankan karna rawan sql injection
+
+echo $sql . PHP_EOL;
+
+$statement = $PDO->query($sql);
+
+$success = false;
+$find_user = null;
+foreach ($statement as $row) {
+    // sukses
+    $success = true;
+    $find_user = $row["username"];
+}
+
+if ($success) {
+    echo "Sukse login : " . $find_user . PHP_EOL;
+} else {
+    echo "Gagal login" . PHP_EOL;
+}
+
+$connection = null;
---
+++ SqlInjectionImprove.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once __DIR__ . '/ConnectionDB.php'; // require_once __DIR__ . '/namaClass.php';
+
+$PDO = getConnection(); // open koneksi ke driver mysql
+
+// tidak di sarankan karna SQL Injection
+// $username = "budioct'; #"; // karna # jadi di belakang query di angap comment
+// $password = "salah gak peduli"; // jadi comment karna variable username ada #
+
+//$username = $PDO->quote("budioct'; #"); // sql injection
+$username = $PDO->quote("budioct"); // quote() // mendeteksi sql injection
+$password = $PDO->quote("pass");
+
+$sql = "SELECT * FROM admin WHERE username = $username AND password = $password";
+
+echo $sql . PHP_EOL;
+
+$statement = $PDO->query($sql);  // query(): PDOStatement // eksekusi query sql fetch data
+
+$success = false;
+$find_user = null;
+foreach ($statement as $row) {
+    // sukses
+    $success = true;
+    $find_user = $row["username"]; // get data dari hasil query ["name_column"]
+}
+
+if ($success) {
+    echo "Sukse login : " . $find_user . PHP_EOL;
+} else {
+    echo "Gagal login" . PHP_EOL;
+}
+
+$connection = null;
+
+
+/**
+ * dengan seperti ini maka tidak akan SQL Injection
+ */
